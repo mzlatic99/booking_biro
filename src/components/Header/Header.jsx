3b88f9ec930cf99a2eba3eecf4fa87b07562e24e
Mateo Zlatić <cromateozlatic@gmail.com>
2024-10-23 16:41:26 +0200

    git init

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div>
+      <ul>
+        <li>Logo</li>
+        <li>Artists</li>
+        <li>Info</li>
+        <li>Let's Talk</li>
+      </ul>
+    </div>
+  )
+}
